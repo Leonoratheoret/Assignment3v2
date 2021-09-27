2cbc0d575ee68204c012b77e06f3a5db363c2b36
Leonoratheoret <70759304+Leonoratheoret@users.noreply.github.com>
2021-09-27 19:33:35 +0200

    some tests to wizard made

--- Assignment3.Tests/QueriesTests.cs
+++ Assignment3.Tests/QueriesTests.cs
@@ -1,10 +1,35 @@
 using System;
 using Xunit;
+using System.Collections;
+using System.Linq;
 
 namespace Assignment3.Tests
 {
     public class QueriesTests
     {
+    
+    
+        [Fact]
+        public void test_Sith_Lords_Year()
+        {
+            Queries q = new Queries();
+            Assert.Equal(1977, q.Sith_Lords_Year());
+        }
+
+         [Fact]
+        public void test_Sith_Lords_Year2()
+        {
+            Queries q = new Queries();
+            Assert.Equal(1977, q.Sith_Lords_Year2());
+        }
+
+         [Fact]
+        public void Order_Wizards_List2()
+        {
+            Queries q = new Queries();
+
+            Assert.Equal(55, q.Order_Wizards_List2().Count());
+        }
        
     }
 }
